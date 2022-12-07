ac0ddb81db10850b004fc8ec3a3a4b8fb28f282c
ShinDongJun <wq0212@naver.com>
2022-12-07 19:20:29 +0900

    feat: Boards 관련 API 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
+import { BoardsModule } from './boards/boards.module';
 
 @Module({
-  imports: [MoviesModule],
+  imports: [MoviesModule, BoardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
